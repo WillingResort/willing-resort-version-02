353eef3162407877be4489eb9cf79592dd4c9f27
Sonam Wangchuk <sonamwangchuk77jr@gmail.com>
2025-03-04 21:39:34 +0600

    chatbot type issue

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -1,7 +1,35 @@
 "use client";
 import { useEffect } from "react";
 
-const JotFormWidget = () => {
+declare global {
+  interface Window {
+    AgentInitializer?: {
+      init: (config: {
+        agentRenderURL: string;
+        rootId: string;
+        formID: string;
+        queryParams: string[];
+        domain: string;
+        isDraggable: boolean;
+        background: string;
+        buttonBackgroundColor: string;
+        buttonIconColor: string;
+        variant: boolean;
+        customizations: {
+          greeting: string;
+          greetingMessage: string;
+          openByDefault: string;
+          pulse: string;
+          position: string;
+          autoOpenChatIn: string;
+        };
+        isVoice: boolean;
+      }) => void;
+    };
+  }
+}
+
+const JotFormWidget: React.FC = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://cdn.jotfor.ms/s/umd/latest/for-embedded-agent.js";
@@ -11,7 +39,8 @@ const JotFormWidget = () => {
     script.onload = () => {
       if (window.AgentInitializer) {
         window.AgentInitializer.init({
-          agentRenderURL: "https://agent.jotform.com/0195619fe6f67dbe9d9b591f9c4adb2c4ef3",
+          agentRenderURL:
+            "https://agent.jotform.com/0195619fe6f67dbe9d9b591f9c4adb2c4ef3",
           rootId: "JotformAgent-0195619fe6f67dbe9d9b591f9c4adb2c4ef3",
           formID: "0195619fe6f67dbe9d9b591f9c4adb2c4ef3",
           queryParams: ["skipWelcome=1", "maximizable=1"],
