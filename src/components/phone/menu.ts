f5de57e864d23c9268e8e04af6d958635e931dae
Sonam Wangchuk <sonamwangchuk77jr@gmail.com>
2025-03-07 14:27:09 +0600

    mobile menu bug fixed

--- src/components/phone/menu.ts
+++ src/components/phone/menu.ts
@@ -3,7 +3,7 @@ export const menuItems = [
   { label: 'ACCOMMODATION', href: '/accommodation', hasIcon: true, submenu: false },
   { label: 'DINING', href: '/dining', hasIcon: true,submenu: false },
   { label: 'WELLNESS', href: '/wellness', hasIcon: true,submenu: false },
-  { label: 'EXPERIENCES', href: '/experience', hasIcon: true ,submenu: true,
+  { label: 'EXPERIENCES', href: '#', hasIcon: true ,submenu: true,
     subLabel: [
       { label: 'HIKING', href: '/experience/hiking' },
       { label: 'VISIT TO VILLAGES', href: '/experience/villages' },
